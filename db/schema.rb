7a8c2d4e27c4e24dc60311ac355d8be2ba0b1212
halloween-jack <halloween.jack.dev@gmail.com>
2019-05-24 22:43:12 +0900

    fix book schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190524115436) do
+ActiveRecord::Schema.define(version: 20190524134056) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20190524115436) do
     t.date "publish_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
   create_table "widgets", force: :cascade do |t|
