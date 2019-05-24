43f302a9603aeb5c612024150e8dada66fa58f50
halloween-jack <halloween.jack.dev@gmail.com>
2019-05-24 22:29:51 +0900

    fix db migration

--- db/migrate/20190524115436_create_books.rb
+++ db/migrate/20190524115436_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.price :price
+      t.integer :price
       t.date :publish_date
 
       t.timestamps
